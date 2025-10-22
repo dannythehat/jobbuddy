6331d7611a194dd883f3a29e680305ea48c29f33
dannythehat <dannythehat2@gmail.com>
2025-10-22 06:59:59 +0300

    Add models index file

---
+++ backend/src/models/index.ts
@@ -0,0 +1,17 @@
+import { User } from './User';
+import { CV } from './CV';
+import { JobPreference } from './JobPreference';
+import { Job } from './Job';
+import { Application } from './Application';
+
+// Export all models
+export {
+  User,
+  CV,
+  JobPreference,
+  Job,
+  Application,
+};
+
+// Note: Associations are defined in each model file
+// This index file is for easy importing of all models
