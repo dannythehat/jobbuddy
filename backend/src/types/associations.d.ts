a7ccbaccdcaeabc944b2dba0591a40f798d5d3af
dannythehat <dannythehat2@gmail.com>
2025-10-31 07:36:34 +0200

    Update associations type definitions with all relationships

--- backend/src/types/associations.d.ts
+++ backend/src/types/associations.d.ts
@@ -9,15 +9,32 @@ declare module '../models/Application' {
   interface Application {
     job?: Job | null;
     cv?: CV | null;
+    responses?: ResponseModel[];
+    interviews?: Interview[];
   }
 }
+
 declare module '../models/Response' {
   interface Response {
     application?: Application | null;
   }
 }
+
 declare module '../models/Interview' {
   interface Interview {
     application?: Application | null;
+    response?: ResponseModel | null;
+  }
+}
+
+declare module '../models/Job' {
+  interface Job {
+    applications?: Application[];
+  }
+}
+
+declare module '../models/CV' {
+  interface CV {
+    applications?: Application[];
   }
 }
